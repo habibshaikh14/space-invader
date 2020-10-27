f04e37d680eb4c3c42276852443661e30d9f2c29
Habib Shaikh <33100185+habibshaikh14@users.noreply.github.com>
2020-10-27 11:53:40 +0530

    - Add enemy health
    - Add enemy Laser
    - Add Damage Dealer

---
+++ Space Invader/Assets/Scripts/Enemy.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Configuration variables
+    [SerializeField] GameObject laserPrefab;
+    [SerializeField] float health = 100f;
+    [SerializeField] float shotCounter;
+    [SerializeField] float minTimeBetweenShots = 0.3f;
+    [SerializeField] float maxTimeBetweenShots = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(MakeEnemyFire());
+        // shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+    }
+
+    private IEnumerator MakeEnemyFire()
+    {
+        while (gameObject)
+        {
+            shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+            yield return new WaitForSeconds(shotCounter);
+            Fire();
+        }
+        
+    }
+
+    private void Fire()
+    {
+        Instantiate(laserPrefab, transform.position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        DamageDealer damageDealer = other.GetComponent<DamageDealer>();
+        ProcessHit(damageDealer);
+    }
+
+    private void ProcessHit(DamageDealer damageDealer)
+    {
+        health -= damageDealer.GetDamage();
+        if (health <= 0f)
+        {
+            Destroy(gameObject);
+        }
+        damageDealer.Hit();
+    }
+}
--- Space Invader/Assets/Scripts/Laser.cs
+++ Space Invader/Assets/Scripts/Laser.cs
@@ -4,10 +4,11 @@ public class Laser : MonoBehaviour
 {
     // Configuration Parameters
     [Range(0f, 20f)][SerializeField] float laserSpeed = 10f;
+    [SerializeField] bool isEnemyLaser = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, laserSpeed);
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, isEnemyLaser ? -laserSpeed : laserSpeed);
     }
 }
